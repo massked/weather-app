65f1f309a7a4be6273278a5798a3d899a5d47a82
Evgeni Minkov <51525675+massked@users.noreply.github.com>
2020-03-03 22:14:01 +0200

    Added post route and fetch post to client

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ const cors = require('cors');
 app.listen(3000, () => console.log('listening on port 3000'));
 app.use(cors());
 app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
+app.post('/api', (req, res) => {
+    console.log(req.body);
+    const data = req.body;
+    res.json({
+        status: 'success',
+        latitude: data.lat,
+        longitute: data.lon
+    });
+});
