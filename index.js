e28a3dcdf237cfb73152b5f874bb87a849206f3a
Evgeni Minkov <51525675+massked@users.noreply.github.com>
2020-03-03 22:50:31 +0200

    Added NeDB as a database source

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Datastore = require('nedb');
 const app = express();
 const cors = require('cors');
 app.listen(3000, () => console.log('listening on port 3000'));
@@ -6,11 +7,18 @@ app.use(cors());
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
+const database = new Datastore('database.db');
+database.loadDatabase();
+
 app.post('/api', (req, res) => {
     console.log(req.body);
     const data = req.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
     res.json({
         status: 'success',
+        timestamp: timestamp,
         latitude: data.lat,
         longitute: data.lon
     });
