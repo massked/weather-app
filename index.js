9cee943dc21e0a922bbe59fc21c0d56d099b48f4
Evgeni Minkov <51525675+massked@users.noreply.github.com>
2020-03-03 17:12:47 +0200

    Get Location Coordinates from Navigator API on Client Side

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 app.listen(3000, () => console.log('listening on port 3000'));
+app.use(cors());
+app.use(express.static('public'));
